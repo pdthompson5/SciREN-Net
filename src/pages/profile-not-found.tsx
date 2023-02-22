deafff105bacfa6d17a814806f530d74a2143dd9
pdthompson5 <pdthompson5@crimson.ua.edu>
2023-02-21 19:51:34 -0600

    Add profile not found page

---
+++ src/pages/profile-not-found.tsx
@@ -0,0 +1,36 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Profile.module.css'
+
+const inter = Inter({ subsets: ['cyrillic'] })
+
+/*
+Created by Phil on 02/21/23
+Page that the user will be redirected to if they attempt to access a profile that does not exist
+*/
+
+export default function ProfileNotFound() {
+  return (
+    <>
+      <Head>
+        <title>SciREN Profile Example</title>
+        <meta name="description" content="Created by Stephen Kirby" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.topbar}>
+          <h1>
+            Profile not found.
+          </h1>
+        </div>
+        <div className={styles.main}>
+          <p>
+            If you believe this to be a mistake, please contact your administrator.
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
