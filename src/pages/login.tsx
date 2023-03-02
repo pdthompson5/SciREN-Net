0d3170e1a12c9e83a6da2e2d6511e7da6bc286da
Stephen Kirby <me@skirby.dev>
2023-03-02 15:52:04 -0600

    moved styles

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import useUser from "@/lib/useUser";
 import fetchJson, { FetchError } from "@/lib/fetchJson";
 import Head from "next/head";
+import styles from "@/styles/Form.module.css";
 
 /* Login Page */
 
@@ -40,7 +41,7 @@ export default function Login() {
       }
     }
   }
-  
+
   return (
     <>
       <Head>
@@ -50,44 +51,24 @@ export default function Login() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1> The login form is below, bro. </h1>
+        <div className={styles.loginTitle}>
+          <h1> SCIRen - Login </h1>
+        </div>
+        <form onSubmit={handleSubmit} className={styles.loginForm}>
+          <label className={styles.loginLabel}>
+            <span className={styles.loginPrompt}>Enter your Email</span>
+            <input type="text" name="email" required className={styles.formInput} />
+          </label>
+          <label className={styles.loginLabel}>
+            <span className={styles.loginPrompt}>Enter your Password</span>
+            <input type="password" name="password" required className={styles.formInput} />
+          </label>
+
+          <button type="submit" className={styles.loginSubmit}>Login</button>
+
+          {errorMsg && <p className={styles.error}>{errorMsg}</p>}
+        </form>
       </div>
-      <form onSubmit={handleSubmit}>
-      <label>
-        <span>Enter your Email</span>
-        <input type="text" name="email" required />
-      </label>
-      <label>
-        <span>Enter your Password</span>
-        <input type="password" name="password" required />
-      </label>
-
-      <button type="submit">Login</button>
-
-      {errorMsg && <p className="error">{errorMsg}</p>}
-
-      <style jsx>{`
-        form,
-        label {
-          display: flex;
-          flex-flow: column;
-        }
-        label > span {
-          font-weight: 600;
-        }
-        input {
-          padding: 8px;
-          margin: 0.3rem 0 1rem;
-          border: 1px solid #ccc;
-          border-radius: 4px;
-        }
-        .error {
-          color: brown;
-          margin: 1rem 0 0;
-        }
-      `}</style>
-    </form>
     </>
-    
   );
 }
