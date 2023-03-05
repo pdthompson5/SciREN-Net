96cee2afafb0b1c847c132c1f533186b2f2a270b
pdthompson5 <pdthompson5@crimson.ua.edu>
2023-03-04 22:34:03 -0600

    Clean up page a little

--- src/pages/profile-not-found.tsx
+++ src/pages/profile-not-found.tsx
@@ -1,20 +1,11 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Profile.module.css'
-
-const inter = Inter({ subsets: ['cyrillic'] })
-
-/*
-Created by Phil on 02/21/23
-Page that the user will be redirected to if they attempt to access a profile that does not exist
-*/
 
 export default function ProfileNotFound() {
   return (
     <>
       <Head>
-        <title>SciREN Profile Not Found</title>
+        <title>SciREN-Net Profile Not Found</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
