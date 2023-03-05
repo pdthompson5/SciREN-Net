541b3ddef69e7cda646573fb53c730e36fbedf74
pdthompson5 <pdthompson5@crimson.ua.edu>
2023-03-04 22:22:41 -0600

    Implment universal header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { SWRConfig } from "swr";
 import fetchJson from "@/lib/fetchJson";
 import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
         },
       }}
     >
+      <Layout/>
       <Component {...pageProps} />
     </SWRConfig>
   );
