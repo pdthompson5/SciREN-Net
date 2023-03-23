0af59b2a6f6c9cf311f0ffb8dc5c27e15e322478
pdthompson5 <pdthompson5@crimson.ua.edu>
2023-03-23 11:36:10 -0500

    Remove unused import

--- src/pages/profiles/[userID].tsx
+++ src/pages/profiles/[userID].tsx
@@ -10,7 +10,6 @@ import {
 } from "@/lib/database";
 import { GetStaticPropsContext } from "next";
 import styles from "@/styles/Profile.module.css";
-import { BSONError } from "bson";
 
 const UserProfile: React.FC<ProfileInformation> = (
   props: ProfileInformation
