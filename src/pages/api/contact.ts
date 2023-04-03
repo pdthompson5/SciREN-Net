0012070276dbb078f2da850e55e374c3f2402aca
pdthompson5 <pdthompson5@crimson.ua.edu>
2023-04-03 10:25:55 -0500

    Improve email structure

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -20,29 +20,28 @@ const transporter = nodemailer.createTransport({
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const request: ContactRequest = await JSON.parse(req.body);
-    console.log(process.env.EMAIL_PASSWORD)
 
     const contactingUserFullName = `${request.contactingUser.firstName} ${request.contactingUser.lastName}`
-    console.log(request.contactedUserEmail)
   
     // send mail with defined transport object
     const info = await transporter.sendMail({
       from: `"${process.env.EMAIL_ADDRESS}"`, // sender address
       to: request.contactedUserEmail, // list of receivers
       subject: `SciRen-Net Contact Request from ${contactingUserFullName}`, // Subject line
-      text: `This is the message: ${request.message}`, // plain text body
-      html: "<b>Hello world?</b>", // html body
+      text: `            
+                Contact request from ${contactingUserFullName} on SciRen-Net
+                Message:
+                ${request.message}
+                To respond to the message, please email Test User at ${request.contactingUser.email}
+                `, // plain text body
+      html: `
+                <h1>Contact request from ${contactingUserFullName} on SciRen-Net</h1>
+                <h2>Message:<h2>
+                <p>${request.message}</p>
+                <h2>To respond to the message, please email Test User at ${request.contactingUser.email}</h2>
+            `,
     });
 
-
     console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-  
-    // Preview only available when sending through an Ethereal account
-    // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-
-
     return res.status(200).json({contacted: true})
-
 }
