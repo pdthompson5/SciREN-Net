18bd41e25e2830045f5b0d78985cf4c0e0e98547
pdthompson5 <71530668+pdthompson5@users.noreply.github.com>
2023-04-23 13:33:28 -0500

    refactor: Update Profile Appearance (#27)
    * Update profile appearance
    
    * Clean up for code review

--- src/pages/profiles/[userID].tsx
+++ src/pages/profiles/[userID].tsx
@@ -10,8 +10,7 @@ import {
 } from "@/lib/database";
 import { GetStaticPropsContext } from "next";
 import styles from "@/styles/Profile.module.css";
-import { Button } from "@mui/material";
-import { gradeRangeOptions } from "../edit-profile";
+import { Box, Button, Divider, List, ListItem, capitalize } from "@mui/material";
 
 const UserProfile: React.FC<ProfileInformation> = (
   props: ProfileInformation
@@ -20,6 +19,7 @@ const UserProfile: React.FC<ProfileInformation> = (
 
   const title = `${props.firstName} ${props.lastName} | SciREN-Net`;
   const isCurrentUser = user && user.isLoggedIn && user.email === props.email;
+  const multipleOrgs = props.organizations.length > 1;
 
   return (
     <>
@@ -29,52 +29,103 @@ const UserProfile: React.FC<ProfileInformation> = (
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.profileBox}>
-        <h1 className="username">
-          <Image
-            height="36"
-            width="36"
-            alt="Profile Picture"
-            className="profile"
-            src="https://github.com/s-kirby.png"
-          />
-          {props.firstName} {props.lastName}
-          {isCurrentUser && (
-            <Link href="/edit-profile">
-              <Image
-                height="24"
-                width="24"
-                alt="Edit Profile"
-                className="profile"
-                src="/edit.svg"
-              ></Image>
-            </Link>
-          )}
-        </h1>
+    <div className={styles.profileBox}>
+        <Box className={styles.profileElement}>
+          <h1 className="username">
+            <Image
+              height="36"
+              width="36"
+              alt="Profile Picture"
+              className="profile"
+              src="https://github.com/s-kirby.png"
+            />
+            {props.firstName} {props.lastName}
+            {isCurrentUser && (
+              <Link href="/edit-profile">
+                <Image
+                  height="24"
+                  width="24"
+                  alt="Edit Profile"
+                  className="profile"
+                  src="/edit.svg"
+                ></Image>
+              </Link>  
+            )}
+            <Divider></Divider>
+          </h1>
+          <h3>
+              {capitalize(props.userType)}  
+              {props.organizations.length > 0 ? ` at ${props.organizations[0]}`: ""}
+          </h3>
+          <h3>
+            Region: {props.scirenRegion}  
+          </h3>
+        </Box>
 
-        <div>
-          <ul>
-            <li>Profile Type: {props.userType}</li>
-            <li>UserID: {props.userID}</li>
-            <li>{props.userType === "researcher" ? "Research Areas" : "Subjects Taught"}: {props.academicInterest.toString()}</li>
-            {props.userType === "teacher" && <li>Grade Range: {props.gradeRange.map((val) => gradeRangeOptions[val]).toString()}</li>}
-            <li>User Type: {props.userType}</li>
-            <li>Organization(s): {props.organizations.toString()}</li>
-            <li>Position(s): {props.position}</li>
-            <li>SciREN Region: {props.scirenRegion}</li>
-            <li>TextBio: {props.textBio}</li>
-            {(user && user.isLoggedIn) && 
-            <li>
-              <Link href={`/contact?user=${props.userID}`} passHref>
-                <Button variant="contained" color="primary">Contact User</Button>
-              </Link>
-            </li>}
-          </ul>
-        </div>
+        <Box className={styles.profileElement}>
+            <h3>About</h3>
+            <Divider></Divider>
+            <p className={styles.textBio}>
+              {props.textBio}
+            </p>
+        </Box>
+
+        <Box className={styles.profileElement}>
+          <h3>{props.userType === "teacher" ? "Subjects Taught" : "Research Areas"}</h3>
+          <Divider></Divider>
+          <List>
+            {getAcademicInterestElements(props.academicInterest)}
+          </List>
+        </Box>
+
+        <Box className={styles.profileElement}>
+          <h3>
+            Affiliated Organization{multipleOrgs ? "s": ""}
+            <Divider></Divider>
+          </h3>
+          <List>
+            {props.organizations.map(
+              (value, index) => (
+                <ListItem key={`organizations-${index}`} disableGutters>
+                  {value}
+                </ListItem>
+              ))
+            }
+          </List>
+          <h3>Position{multipleOrgs ? "s": ""} at Organization{multipleOrgs ? "s": ""}</h3>
+          <Divider></Divider>
+          <p className={styles.profileParagraph}>{props.position}</p>
+        </Box>
+
+        <Box className={styles.profileElement}>
+            <h3>Lesson Plans</h3>
+            <Divider></Divider>
+            <p className={styles.profileParagraph}>This is where the linked lesson plans will be</p>
+        </Box>
+        <Box className={styles.profileElement}>
+            <h3>External Links</h3>
+            <p className={styles.profileParagraph}>This is where external links will go once that is implemented</p>
+        </Box>
+
+        {(user && user.isLoggedIn) &&               
+          <Link href={`/contact?user=${props.userID}`} passHref>
+            <Button variant="contained">Contact User</Button>
+          </Link>
+        }
       </div>
     </>
   );
 };
+
+const getAcademicInterestElements = (academicInterest: string[]) => {
+  return academicInterest.map(
+    (value, index) => (
+      <ListItem disableGutters key={`academic-interest-${index}`}>
+        {value}
+      </ListItem>
+    )
+  )
+}
 
 //Return props for each page
 export const getStaticProps = async ({
